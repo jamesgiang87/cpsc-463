675f26cf1ab0c637b6ca82eec4084d01b910793d
walidelmir <33739221+walidelmir@users.noreply.github.com>
2019-05-05 15:10:14 -0700

    final sql

--- tableflipper.sql
+++ tableflipper.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 25, 2019 at 11:45 PM
+-- Generation Time: May 06, 2019 at 12:08 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -73,9 +73,40 @@ CREATE TABLE `INVENTORY` (
 --
 
 INSERT INTO `INVENTORY` (`BARC`, `NUM`, `NAME`, `TYPE`, `INFO`, `BUYPRICE`, `SELLPRICE`, `image`) VALUES
-(1111111, '222222', 'apple watch', 'watch', '44mm', 100, 300, 'apple-watch.jpeg'),
-(123, '322', 'Iphone X plus ', 'phone', 'gold 128GB', 800, 1100, 'iphonex.jpeg'),
-(1234, '4321', 'Apple Watch series 3', 'smart watch', '44mm', 150, 350, 'apple-watch.jpeg');
+(111, '111', '111', '111', '11', 11, 11, 'iphonex.jpeg'),
+(1111111, '222222', 'apple watch', 'watch', '44mm', 100, 300, 'apple-watch.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `username` text NOT NULL,
+  `Email` text NOT NULL,
+  `FirstName` text NOT NULL,
+  `LastName` text NOT NULL,
+  `Address` text NOT NULL,
+  `Apartment` text NOT NULL,
+  `City` text NOT NULL,
+  `Country` text NOT NULL,
+  `State` text NOT NULL,
+  `ZIP` int(11) NOT NULL,
+  `CardNumber` int(11) NOT NULL,
+  `NameOnCard` text NOT NULL,
+  `Expiration` text NOT NULL,
+  `Code` int(11) NOT NULL,
+  `Total` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`username`, `Email`, `FirstName`, `LastName`, `Address`, `Apartment`, `City`, `Country`, `State`, `ZIP`, `CardNumber`, `NameOnCard`, `Expiration`, `Code`, `Total`) VALUES
+('customer', '', '', '', '', '', '', '', '', 0, 0, '', '', 0, 604.95),
+('customer', '', '', '', '', '', '', '', '', 0, 0, '', '', 0, 304.95);
 
 -- --------------------------------------------------------
 
@@ -103,8 +134,22 @@ CREATE TABLE `sales` (
 --
 
 INSERT INTO `sales` (`SALES_NUM`, `BARCODE`, `PURCHASE_DATE`, `CUSTOMER_EMAIL`, `BUYPRICE`, `INFO`, `MARKPRICE`, `NAME`, `NUM`, `SELLPRICE`, `TYPE`, `USERNAME`) VALUES
-(1113040625, 1234567, '2018-12-10', 'customer@gmail.com', 100, 'black 44mm', 0, 'Apple watch series 3', '7654321', 300, 'smart watch', 'customer'),
-(913203308, 123, '2018-12-10', 'customer@gmail.com', 700, '125GB', 0, 'iPhone X', '321', 1000, 'phone', 'customer');
+(1247176432, 1111111, '2019-05-05', '', 100, '44mm', 0, 'apple watch', '222222', 300, 'watch', 'customer'),
+(1155419907, 1111111, '2019-05-05', '', 100, '44mm', 0, 'apple watch', '222222', 300, 'watch', 'customer'),
+(1318883590, 1111111, '2019-05-05', '', 100, '44mm', 0, 'apple watch', '222222', 300, 'watch', 'customer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shoppingcart`
+--
+
+CREATE TABLE `shoppingcart` (
+  `username` text NOT NULL,
+  `NAME` text NOT NULL,
+  `num` int(11) NOT NULL,
+  `PRICE` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -128,13 +173,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `user_level`, `image`, `status`, `last_login`) VALUES
-(1, ' Admin User', 'admin', '6c7ca345f63f835cb353ff15bd6c5e052ec08e7a', 1, 'no_image.jpg', 1, '2019-04-25 21:26:14'),
-(2, 'Special User', 'special', 'ba36b97a41e7faf742ab09bf88405ac04f99599a', 2, 'no_image.jpg', 1, '2018-12-03 21:03:18'),
-(3, 'Default User', 'user', '12dea96fec20593566ab75692c9949596833adc9', 3, 'no_image.jpg', 1, '2018-12-03 20:47:18'),
-(22, 'Employee User', 'danny', 'df4bf92e16bbb7396c50d637f6b7ad9a4c8c274c', 2, 'no_image.jpg', 1, NULL),
-(55, 'Employee User', 'james', '474ba67bdb289c6263b36dfd8a7bed6c85b04943', 2, 'no_image.jpg', 1, '2018-12-10 20:15:23'),
-(12345, 'Employee User', 'da', 'cdd4f874095045f4ae6670038cbbd05fac9d4802', 2, 'no_image.jpg', 1, '2018-12-10 20:37:22'),
-(12346, 'Customer User', 'customer', 'b39f008e318efd2bb988d724a161b61c6909677f', 3, 'no_image.jpg', 1, '2019-04-25 23:41:16'),
+(1, ' Admin User', 'admin', '6c7ca345f63f835cb353ff15bd6c5e052ec08e7a', 1, 'no_image.jpg', 1, '2019-05-05 23:49:35'),
+(12346, 'Customer User', 'customer', 'b39f008e318efd2bb988d724a161b61c6909677f', 3, 'no_image.jpg', 1, '2019-05-05 23:54:11'),
 (12347, 'Customer User', 'customer1', 'e2ea3c6b50c654e7c809c252b97d94386fb283fc', 3, 'no_image.jpg', 1, '2018-12-09 21:17:08'),
 (12348, 'Customer User', 'www', 'c50267b906a652f2142cfab006e215c9f6fdc8a0', 3, 'no_image.jpg', 1, '2018-12-10 20:25:15');
 
